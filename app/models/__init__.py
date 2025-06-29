dbb2d980066fe620df240817376d73498979866d
Akshay Tejwani <tejwaniakshay710@gmail.com>
2025-06-29 19:23:17 +0530

    Turn-10-A added the unit test suites for verifying the models and schemas

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -6,10 +6,10 @@
 # from app.models.workout_session import WorkoutSession
 # from app.models.set_log import SetLog
 from app.models.assignment import Assignment
-# from app.models.session import Session
-# from app.models.nutrition_goal import NutritionGoal
-# from app.models.exercise_log import ExerciseLog
-# from app.models.workout_plan import WorkoutPlan
+from app.models.session import Session
+from app.models.nutrition_goal import NutritionGoal
+from app.models.exercise_log import ExerciseLog
+from app.models.workout_plan import WorkoutPlan
 from app.models.user import User
 from app.models.workout import Workout
 from app.models.exercise import Exercise
